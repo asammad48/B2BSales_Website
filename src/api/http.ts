7dae65f56d18955ed29650a7caf34aabbe4aea00
Abdul Sammad <asammad48@gmail.com>
2026-03-14 21:47:50 +0500

    Add X-Tenant-Id header to all API requests

--- src/api/http.ts
+++ src/api/http.ts
@@ -7,9 +7,11 @@ export const http = axios.create({
 });
 
 http.interceptors.request.use((config) => {
+  config.headers = config.headers || {};
+  config.headers['X-Tenant-Id'] = '6ee5fe60-d3ae-4f7f-870f-155bfd6ebd18';
+
   const token = localStorage.getItem('buyer_access_token');
   if (token) {
-    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
