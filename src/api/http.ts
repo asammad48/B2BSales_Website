4aae675a90d23990e32e415143531b82624edbba
Abdul Sammad <asammad48@gmail.com>
2026-03-14 05:39:05 +0500

    Fix API response mapping and auth token propagation

--- src/api/http.ts
+++ src/api/http.ts
@@ -3,11 +3,13 @@ import { env } from '@/env';
 
 export const http = axios.create({
   baseURL: env.apiBaseUrl,
+  transformResponse: [(data) => data],
 });
 
 http.interceptors.request.use((config) => {
   const token = localStorage.getItem('buyer_access_token');
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
