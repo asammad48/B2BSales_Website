d92288eb256b9c624706ba6c1eea80f2e7f4d0ef
Abdul Sammad <Abdul Sammad@DESKTOP-L7JHC1H>
2026-03-30 00:25:05 +0500

    Changes in env.ts file

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const env = {
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL || '',
-  tenantId: import.meta.env.VITE_TENANT_ID || '6ee5fe60-d3ae-4f7f-870f-155bfd6ebd18',
+  tenantId: import.meta.env.VITE_TENANT_ID || '',
 };
